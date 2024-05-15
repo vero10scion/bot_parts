e17bae877e2f79487777726a404df8feefb71db5
vero10scion <133650868+vero10scion@users.noreply.github.com>
2024-05-15 20:50:00 +0300

    Delete trying.py

--- trying.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-connection = sqlite3.connect('sss.db')
-cursor = connection.cursor()
-cursor.execute('SELECT ur_name FROM comp WHERE ur_name LIKE "%МФК%"')
-res = cursor.fetchone()
-print(res)
